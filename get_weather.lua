9cd71f557a5bc19bbb8c4bea9d11cfd4f916a987
dannythecoder <dan@coddledsoftware.com>
2016-01-14 20:35:31 -0500

    Started implementing the weather parser.

--- get_weather.lua
+++ get_weather.lua
@@ -17,7 +17,9 @@
 -- in Florida.
 
 lat = "28.3714"
-long = "-81.548"
+lon = "-81.548"
+
+debug = true
 
 -- During testing, load from sample_weather.dat (instead of getting
 -- the latest version from the website).
@@ -38,11 +40,24 @@ end
   ]]
 function parseContent(content)
     result = {}
+
+    tempMinStart = string.find(content, "Daily Minimum Temperature")
+    tempMaxStart = string.find(content, "Daily Maximum Temperature")
     
-    result["Today"] = 76
-    result["Tomorrow"] = 72
-    result["Today + 2"] = 71
+    if tempMinStart == nil or tempMaxStart == nil then
+        return nil
+    end
+
+    result["Lo Today   "] = string.sub(content, tempMinStart + 26, tempMinStart + 27)
+    result["Lo Tomorrow"] = string.sub(content, tempMinStart + 29, tempMinStart + 30)
+    result["Lo Tomorr+1"] = string.sub(content, tempMinStart + 32, tempMinStart + 33)
+    result["Lo Tomorr+2"] = string.sub(content, tempMinStart + 35, tempMinStart + 36)
     
+    result["Hi Today   "] = string.sub(content, tempMaxStart + 26, tempMaxStart + 27)
+    result["Hi Tomorrow"] = string.sub(content, tempMaxStart + 29, tempMaxStart + 30)
+    result["Hi Tomorr+1"] = string.sub(content, tempMaxStart + 32, tempMaxStart + 33)
+    result["Hi Tomorr+2"] = string.sub(content, tempMaxStart + 35, tempMaxStart + 36)
+
     return result
 end
 
@@ -55,8 +70,20 @@ function getContent()
         local file = assert(io.open("sample_weather.dat", "r"))
         content = file:read()
         file:close()
+    else
+        -- Get the content from a website
+        local url = "http://forecast.weather.gov/MapClick.php?lat=" .. lat .. "&lon=" .. lon .. "&unit=0&lg=english&FcstType=dwml"
+
+        if debug then
+            print(url)
+        end
+
+        local http = require'socket.http'
+        body, c, l, h = http.request(url)
+        content = body
+
     end
-    
+
     return content
 end
 
@@ -65,6 +92,11 @@ end
 content = getContent()
 weather = parseContent(content)
 
--- For now, just display the content
-print(content)
+-- For now, just display the content and parsed version
+if debug then
+    print(content)
+    print("\n\n")
+end
+
+for key, value in pairs(weather) do print(key, value) end
 
